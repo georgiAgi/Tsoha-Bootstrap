81630a9096f9dd78a58f941f36c0891b4b5849e1
Georgi Agiashvili <gexgex@dhcp-asv-126.helsinki.fi>
2017-01-26 22:46:44 +0200

    Tokan viikon hotmölöt tehty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,50 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('home.html');
+        ;
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('vote_edit.html');
     }
-  }
+
+    public static function candidateEdit() {
+        // Testaa koodiasi täällä
+        View::make('candidate_edit.html');
+    }
+
+    public static function candidateShow() {
+        // Testaa koodiasi täällä
+        View::make('candidate_show.html');
+    }
+
+    public static function userEdit() {
+        // Testaa koodiasi täällä
+        View::make('user_edit.html');
+    }
+    public static function userShow() {
+        // Testaa koodiasi täällä
+        View::make('user_show.html');
+    }
+
+    public static function voteEdit() {
+        // Testaa koodiasi täällä
+        View::make('vote_edit.html');
+    }
+
+    public static function voteShow() {
+        // Testaa koodiasi täällä
+        View::make('vote_show.html');
+    }
+
+    public static function voteList() {
+        // Testaa koodiasi täällä
+        View::make('vote_list.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,37 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/candidate_edit', function() {
+    HelloWorldController::candidateEdit();
+});
+
+  $routes->get('/candidate_show', function() {
+    HelloWorldController::candidateShow();
+});
+
+  $routes->get('/user_edit', function() {
+    HelloWorldController::userEdit();
+});
+
+  $routes->get('/user_show', function() {
+    HelloWorldController::userShow();
+});
+
+  $routes->get('/vote_edit', function() {
+    HelloWorldController::voteEdit();
+});
+
+  $routes->get('/vote_show', function() {
+    HelloWorldController::voteShow();
+});
+
+  $routes->get('/vote_list', function() {
+    HelloWorldController::voteList();
+});
