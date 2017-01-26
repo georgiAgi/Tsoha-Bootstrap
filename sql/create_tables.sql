a5fd22e70889ea604798c66468e1bcff11ed5039
Georgi Agiashvili <gexgex@dhcp-asv-126.helsinki.fi>
2017-01-27 00:12:54 +0200

    Lisätty tietokantataulujen luonti- ja poistokomennot sekä korjattu dokumentaatio

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,33 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+  nimi varchar(100) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+  tiedot varchar(1000)
+);
+
+CREATE TABLE Aanestys(
+  id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+  nimi varchar(100) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+  jarjestaja_id INTEGER REFERENCES Kayttaja(id),
+  lisatieto varchar(1000),
+  alkamisaika DATE,
+  loppumisaika DATE
+);
+
+CREATE TABLE Aanestajalista(
+  kayttaja_id INTEGER REFERENCES Kayttaja(id),
+  aanestys_id INTEGER REFERENCES Aanestys(id)
+);
+
+CREATE TABLE Ehdokas(
+  id SERIAL PRIMARY KEY,
+  nimi varchar(100) NOT NULL,
+  lisatieto varchar(1000),
+  aanestys_id INTEGER REFERENCES Aanestys(id),
+);
+
+CREATE TABLE Aani(
+  id SERIAL PRIMARY KEY,
+  ehdokas_id INTEGER REFERENCES Ehdokas(id),
+  aanestys_id INTEGER REFERENCES Aanestys(id),
+  aika DATE
+);
