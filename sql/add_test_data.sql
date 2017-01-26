c6660809746a26fabf615764dab6ddc68085d399
Georgi Agiashvili <gexgex@dhcp-asv-126.helsinki.fi>
2017-01-27 00:39:26 +0200

    Viimeiset viilaukset, testidata ei toimi jostain syystä

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,4 +1,3 @@
 INSERT INTO Kayttaja (nimi, tiedot) VALUES ('Armas Hella', 'Tekoälyllinen kääpiö');
-INSERT INTO Aanestys (nimi, lisatieto, alkamisaika, loppumisaika) VALUES ('Miss Tekotin', 'Onko hän vekotin', NOW(), '2017-02-14';
-
+INSERT INTO Aanestys (nimi, lisatieto, alkamisaika, loppumisaika) VALUES ('Miss Tekotin', 'Onko hän vekotin', NOW(), NOW());
 INSERT INTO Ehdokas (nimi, lisatieto) VALUES ('C-3PO', 'Huima käännösjäbä');
