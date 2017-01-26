1fe699447538b45a7cfbb40b38713b8fab4b4a76
Georgi Agiashvili <gexgex@dhcp-asv-126.helsinki.fi>
2017-01-27 00:26:13 +0200

    Lisätty testidataa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (nimi, tiedot) VALUES ('Armas Hella', 'Tekoälyllinen kääpiö');
+INSERT INTO Aanestys (nimi, lisatieto, alkamisaika, loppumisaika) VALUES ('Miss Tekotin', 'Onko hän vekotin', NOW(), '2017-02-14';
+
+INSERT INTO Ehdokas (nimi, lisatieto) VALUES ('C-3PO', 'Huima käännösjäbä');
